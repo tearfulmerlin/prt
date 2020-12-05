07b438db36f922bb77665b56973ba55b28258bd4
Oleksii Onyshchuk <oleksii.onyshchuk@additiv.com>
2020-12-05 20:03:13 +0200

    initial commit

---
+++ wp-content/themes/radio_tech/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * radio_tech Theme Customizer
+ *
+ * @package radio_tech
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function radio_tech_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'radio_tech_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'radio_tech_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'radio_tech_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function radio_tech_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function radio_tech_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function radio_tech_customize_preview_js() {
+	wp_enqueue_script( 'radio_tech-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'radio_tech_customize_preview_js' );
